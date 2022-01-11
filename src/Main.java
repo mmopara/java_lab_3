87b14500fa72ebfd6caa73a702745e51212c3b69
mmopara <mmopara@gmail.com>
2022-01-11 11:07:24 +0100

    cw 1

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,58 @@ public class Main {
         do podanych cyfr przez użytkownika,
         g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
         */
+        Scanner scan = new Scanner(System.in);
 
-        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
-        * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
+
+        enum LiczbyEnum {
+            JEDEN, DWA, TRZY, CZTERY, PIEC, SZESC
+        }
+
+        enum StatusEnum {
+            KONTYNUUJEMY, KONIEC
+        }
+
+        while (true) {
+            System.out.println("Podaj liczbe: ");
+            int liczba = scan.nextInt();
+            switch (liczba) {
+                case 0 -> System.out.println(StatusEnum.KONIEC);
+                case 1 -> {
+                    System.out.println(LiczbyEnum.JEDEN);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    continue;
+                }
+                case 2 -> {
+                    System.out.println(LiczbyEnum.DWA);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    continue;
+                }
+                case 3 -> {
+                    System.out.println(LiczbyEnum.TRZY);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    continue;
+                }
+                case 4 -> {
+                    System.out.println(LiczbyEnum.CZTERY);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    continue;
+                }
+                case 5 -> {
+                    System.out.println(LiczbyEnum.PIEC);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    continue;
+                }
+                case 6 -> {
+                    System.out.println(LiczbyEnum.SZESC);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    continue;
+                }
+            }
+            if (liczba == 0) {
+                break;
+            }
+
+        }
 
     }
 }
