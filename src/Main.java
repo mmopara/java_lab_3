d0798618bc741a97ec9ef767df0721c078e44bad
mmopara <mmopara@gmail.com>
2022-01-11 11:23:23 +0100

    cw 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +15,58 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+
+        Student student = new Student();
+        student.imie = "Maciej";
+        student.nazwisko = "Opara";
+        student.wiek = 29;
+        student.aktywny = true;
+
+        List<Object> student1 = new ArrayList<>();
+        student1.add(student.imie);
+        student1.add(student.nazwisko);
+        student1.add(student.wiek);
+        student1.add(student.aktywny);
+
+        for (Object lista : student1) {
+            System.out.println(lista);
+        }
+
+        Student Student2 = new Student();
+        student.imie = "Kamil";
+        student.nazwisko = "Gralik";
+        student.wiek = 30;
+        student.aktywny = true;
+
+        List<Object> student2 = new ArrayList<>();
+        student2.add(student.imie);
+        student2.add(student.nazwisko);
+        student2.add(student.wiek);
+        student2.add(student.aktywny);
+
+        System.out.println("\n");
+
+        for (Object lista : student2) {
+            System.out.println(lista);
+        }
+
+        Student studentC = new Student();
+        studentC.imie = "Grzegprz";
+        studentC.nazwisko = "Krychowiak";
+        studentC.wiek = 32;
+        studentC.aktywny = false;
+
+        List<Object> student3 = new ArrayList<>();
+        student3.add(studentC.imie);
+        student3.add(studentC.nazwisko);
+        student3.add(studentC.wiek);
+        student3.add(studentC.aktywny);
+
+        System.out.println("\n");
+
+        for (Object lista : student3) {
+            System.out.println(lista);
+        }
+
     }
 }
